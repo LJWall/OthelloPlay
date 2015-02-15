4424221549840e57ae95a40e76bcbb030dc15244
Liam Wall <liam.wall@gmail.com>
2015-02-15 16:09:00 +0000

    Raising correct errors and some mild tidying

--- othelo.py
+++ othelo.py
@@ -1,13 +1,18 @@
 
+class InvalidMoveError(Exception):
+    pass
+
 class OtheloBoardClass(dict):
     def __init__(self, size):
+        self.players = ('X', 'O')
         self._size = size
         x = size / 2
-        self[(x-1, x-1)] = "O"
-        self[(x, x)] = "O"
-        self[(x-1, x)] = "X"
-        self[(x, x-1)] = "X"
-            
+        self[(x-1, x-1)] = self.players[1]
+        self[(x, x)] = self.players[1]
+        self[(x-1, x)] = self.players[0]
+        self[(x, x-1)] = self.players[0]
+        self.current_turn = self.players[0]
+        
     
     def __str__(self):
         ret = ''
@@ -22,8 +27,9 @@ class OtheloBoardClass(dict):
         if x<0 or x>=self._size or y<0 or y>=self._size:
             raise KeyError
         if self.get((x,y), 0):
-            raise KeyError # should be another error
-        self[(x,y)] = colour
+            raise InvalidMoveError
+        self[(x,y)] = self.current_turn
+        self.current_turn = self.players[1 - self.players.index(self.current_turn)]
     
 
 if __name__ == '__main__':
