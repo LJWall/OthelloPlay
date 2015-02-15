2b4994686dd65490f3c7aed5935d1a26932e6ad7
Liam Wall <liam.wall@gmail.com>
2015-02-15 15:35:25 +0000

    Test board creation in correct initial state (and __str__ function). Test current player tracking.

--- test_othelo.py
+++ test_othelo.py
@@ -2,8 +2,20 @@ from othelo import OtheloBoardClass
 import unittest # import TestCase, main
 
 class TestOtheloBoardClass(unittest.TestCase):
-    pass
-
-
+    def test_board_creation(self):
+        game = OtheloBoardClass(6)
+        self.assertEqual(str(game), '- - - - - - \n' +
+                                    '- - - - - - \n' +
+                                    '- - O X - - \n' +
+                                    '- - X O - - \n' +
+                                    '- - - - - - \n' +
+                                    '- - - - - - \n')
+    def test_whose_go(self):
+        game = OtheloBoardClass(6)
+        self.assertEqual(game.current_turn, 'X')
+        game.play_move(0, 2)
+        self.assertEqual(game.current_turn, 'O')
+        
+        
 if __name__ == '__main__':
     unittest.main()
