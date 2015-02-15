a3716d3e72be1899197cfbfffcb8c6e02459d54f
Liam Wall <liam.wall@gmail.com>
2015-02-15 15:19:37 +0000

    Initial file Othelo unit test

---
+++ test_othelo.py
@@ -0,0 +1,9 @@
+from othelo import OtheloBoardClass
+import unittest # import TestCase, main
+
+class TestOtheloBoardClass(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
