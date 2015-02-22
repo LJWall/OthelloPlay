6bdb83354b31994b2df05f29783da2db27a6487e
Liam Wall <liam.wall@gmail.com>
2015-02-22 10:28:29 +0000

    Page rendering a board based on api request.  Also binding click event to the board.

--- static/othello.js
+++ static/othello.js
@@ -1,30 +1,38 @@
 
 function OthelloModelView() {
     var self = this;
-    self.womble = ko.observable("Foo bar spam");
-    self.myList = ko.observableArray([1, 2, 3, 4]);
-    self.myList2 = ko.observableArray([1, 2, 3, 4]);
+    self.pieceColours = ko.observableArray();
+    self.pieceSize = ko.observable(60);
+    self.boardSize = ko.observable(4);
     
-    self.pieceColours = ko.observableArray([["rgb(0,100,00)","rgb(0,100,00)","rgb(0,100,00)","rgb(0,100,00)"],
-                                            ["rgb(0,100,00)","black","rgb(200,200,200)","rgb(0,100,00)"],
-                                            ["rgb(0,100,00)","rgb(200,200,200)","black","rgb(0,100,00)"],
-                                            ["rgb(0,100,00)","rgb(0,100,00)","rgb(0,100,00)","rgb(0,100,00)"]]);
-    
-    self.someColour = ko.observable("pink");
-    self.someHTML = ko.observable("<b>Foo!</b>");
-    
-    self.someFunc = function() {
-        if (self.someColour()=="black") {
-            self.someColour("rgb(200,200,200)");
+    self.loadResponse = function(data)
+    {
+        colours = new Array();
+        for (i=0; i<data['size']; i++) {
+            colours[i]= new Array();
+            for (j=0; j<data['size']; j++) {
+                colours[i][j] = "rgb(0,100,00)";
+            }
         }
-        else
-        {
-            self.someColour("black");
+        x = 0;
+        for (i=0; i < data['X'].length; i++) {
+            x = x+ 1;
+            colours[data['X'][i][0]][data['X'][i][1]] = "black";
         }
+        for (i=0; i < data['O'].length; i++) {
+            x = x + 1;
+            colours[data['O'][i][0]][data['O'][i][1]] = "rgb(200,200,200)";
+        }
+        self.boardSize(data['size']);
+        self.pieceColours(colours);
+        self.pieceSize(500/data['size'])
     };
     
-    self.clickPiece = function() {
-        alert('Wombles!');  
+    $.getJSON('/game/40481/1', self.loadResponse);
+
+    
+    self.clickPiece = function(x, y) {
+        alert("x=" + x + "; y=" + y);  
     };
 }
 
